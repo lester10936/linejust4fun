7fed1522ea865b854c5bd839f4b287b4788e4e09
lester10936 <lester10936@gmail.com>
2017-07-25 09:51:09 +0800

    test

--- callback.php
+++ callback.php
@@ -1,69 +1,41 @@
 <?php
+$access_token ='f3Vg+J/EcLYaKSQ3vbCw8/S0h+ggTK2tFITtjqzfe/fx6v/pNbsCqHMWaW5+FkGqNwzDN++BVLK9kNPO0E7Mc4xHMeNOLZrfSYtGUWIjuLAx6UCbQjpW4OfZ5dPkkDx2AeEFHLef5InYP9pZzR6SagdB04t89/1O/w1cDnyilFU=';
+//define('TOKEN', '你的Channel Access Token');
 
-	/* 輸入申請的Line Developers 資料  */
-	$channel_id = "1525875951";
-	$channel_secret = "e7d3d6bb9f69afa1eeaa625ed9530b98";
-	$channel_access_token = "f3Vg+J/EcLYaKSQ3vbCw8/S0h+ggTK2tFITtjqzfe/fx6v/pNbsCqHMWaW5+FkGqNwzDN++BVLK9kNPO0E7Mc4xHMeNOLZrfSYtGUWIjuLAx6UCbQjpW4OfZ5dPkkDx2AeEFHLef5InYP9pZzR6SagdB04t89/1O/w1cDnyilFU=";
+$json_string = file_get_contents('php://input');
 
-	$myURL = "https://linejust4fun.herokuapp.com/"
+$file = fopen("C:\\Line_log.txt", "a+");
+fwrite($file, $json_string."\n"); 
+$json_obj = json_decode($json_string);
 
+$event = $json_obj->{"events"}[0];
+$type  = $event->{"message"}->{"type"};
+$message = $event->{"message"};
+$reply_token = $event->{"replyToken"};
+        
+$post_data = [
+  "replyToken" => $reply_token,
+  "messages" => [
+    [
+      "type" => "text",
+      "text" => $message->{"text"}
+    ]
+  ]
+]; 
+fwrite($file, json_encode($post_data)."\n");
 
-//  當有人發送訊息給bot時 我們會收到的json
-// 	{
-// 	  "events": 
-// 	  [
-// 		  {
-// 			"replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
-// 			"type": "message",
-// 			"timestamp": 1462629479859,
-// 			"source": {
-// 				 "type": "user",
-// 				 "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
-// 			 },
-// 			 "message": {
-// 				 "id": "325708",
-// 				 "type": "text",
-// 				 "text": "Hello, world"
-// 			  }
-// 		  }
-// 	  ]
-// 	}
-	 
-	 
-	// 將收到的資料整理至變數
-	$receive = json_decode(file_get_contents("php://input"));
-	
-	// 讀取收到的訊息內容
-	$text = $receive->events[0]->message->text;
-	
-	// 讀取訊息來源的類型 	[user, group, room]
-	$type = $receive->events[0]->source->type;
-	
-	// 由於新版的Messaging Api可以讓Bot帳號加入多人聊天和群組當中
-	// 所以在這裡先判斷訊息的來源
-	if ($type == "room")
-	{
-		// 多人聊天 讀取房間id
-		$from = $receive->events[0]->source->roomId;
-	} 
-	else if ($type == "group")
-	{
-		// 群組 讀取群組id
-		$from = $receive->events[0]->source->groupId;
-	}
-	else
-	{
-		// 一對一聊天 讀取使用者id
-		$from = $receive->events[0]->source->userId;
-	}
-	
-	// 讀取訊息的型態 [Text, Image, Video, Audio, Location, Sticker]
-	$content_type = $receive->events[0]->message->type;
-	
-	// 準備Post回Line伺服器的資料 
-	$header = ["Content-Type: application/json", "Authorization: Bearer {" . $channel_access_token . "}"];
-	
-	// 回覆訊息
-	reply($content_type, $text);
-	}
+$ch = curl_init("https://api.line.me/v2/bot/message/reply");
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Authorization: Bearer '.$access_token
+    //'Authorization: Bearer '. TOKEN
+));
+$result = curl_exec($ch);
+fwrite($file, $result."\n");  
+fclose($file);
+curl_close($ch); 
 ?>
